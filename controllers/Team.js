672a4e8769af5bbfbc72b3d1be2be3388a37e554
Pebble <limrelena08@gmail.com>
2023-06-13 13:03:28 +0200

    WIP teams routes/controllers (create get done)

--- controllers/Team.js
+++ controllers/Team.js
@@ -1,16 +1,64 @@
 import mongoose from "mongoose";
-import Team from "../models/Team";
+import Team from "../models/Team.js";
+import express from "express";
 
 export const getTeams = async (req,res) => {
+    try {
+        const teams = await Team.find();
+        if (teams) {
+            res.status(200).json(teams);
+            console.log("Get teams success");
+        }
+    } catch (error) {
+        res.status(500).json({ message: error.message })
+        console.log("Error: teams not found");
+    }
+}
 
+// à corriger
+export const getTeam = async (req,res) => {
+    try {
+        const { id } = req.params.id;
+        console.log(id)
+        const team = await Team.findById(id);
+        if (team) {
+            res.status(200).json(team);
+            console.log("Get team success");
+        }
+    } catch (error) {
+        res.status(500).json({ message: error.message })
+        console.log("Error: team not found");
+    }
 }
 
 export const createTeam = async (req,res) => {
-
+    try {
+        const team = req.body;
+        console.log(team)
+        if (team) {
+            const newTeam = new Team(team);
+            console.log(newTeam)
+            await newTeam.save();
+            res.status(201).json(newTeam);
+            console.log("Create team success");
+        }
+    } catch (error) {
+        res.status(400).json({ message: error.message });
+        console.log("Error: team not created");
+    }
 }
 
 export const updateTeam = async (req,res) => {
+    try {
+        const { id } = req.params.id;
+        const team = req.body;
+        if (team) {
 
+        }
+    } catch (error) {
+        res.status(400).json({ message: error.message });
+        console.log("Error: team not updated");
+    }
 }
 
 export const deleteTeam = async (req,res) => {
--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 import express from "express";
-import bodyParser from "body-parser";
 import cors from "cors";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import teamRoutes from "./routes/Team.js";
+import bodyParser from "body-parser";
 
 const app = express();
 dotenv.config();
 
 const PORT = process.env.PORT || 5000;
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use("/teams/", teamRoutes);
 
 mongoose
   .connect(process.env.CONNECTION_URL, {
